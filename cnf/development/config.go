0b18e046fa7ea22ddfca82bcd2ffef8fe35fdbd8
namlevu <namvule@hotmail.com>
2018-11-06 21:36:17 +0900

    remove unuse file

--- cnf/development/config.go
+++
@@ -1,8 +0,0 @@
-package development
-
-const (
-	MONGODB_HOST            = "mongodb://127.0.0.1:27017"
-	MONGODB_DATABASE        = "vblog"
-	MONGODB_CONNECTION_POOL = 5
-	API_PORT                = 9009
-)
--- cnf/production/config.go
+++
@@ -1,3 +0,0 @@
-package production
-
-// TODO: Tobe implements
